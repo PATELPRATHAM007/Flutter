954bdd8ad62b21d528e78115e34a1ff6e8691f98
PATEL PRATHAM <98813916+PATELPRATHAM007@users.noreply.github.com>
2024-01-25 20:10:02 +0530

    quiz app

--- flutter_quiz_app/lib/questions_screen.dart
+++ flutter_quiz_app/lib/questions_screen.dart
@@ -54,6 +54,7 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             const SizedBox(height: 30),
             ...currentQuestion.getShuffledAnswers().map((answer) {
               return AnswerButton(
+                  
                 answerText: answer,
                 onTap: () {
                   answerQuestion(answer);
